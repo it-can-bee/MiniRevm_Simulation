c85a4b49d769a5f621f131a9a053b78354633619
zhangxucan <1712997354@qq.com>
2024-09-03 18:16:48 +0800

    add EvmContext and finish a guideline for system operate

--- rust_simulate_evm/target/debug/deps/lib_utils-b075c16c6522fa08.d
+++ rust_simulate_evm/target/debug/deps/lib_utils-b075c16c6522fa08.d
@@ -1,8 +1,8 @@
-/Users/liangchaohuang/Desktop/MiniRevm_Simulation/rust_simulate_evm/target/debug/deps/liblib_utils-b075c16c6522fa08.rmeta: crates/libs/lib_utils/src/lib.rs crates/libs/lib_utils/src/error.rs
+/Users/liangchaohuang/Desktop/zxc/MiniRevm_Simulation/rust_simulate_evm/target/debug/deps/liblib_utils-b075c16c6522fa08.rmeta: crates/libs/lib_utils/src/lib.rs crates/libs/lib_utils/src/error.rs
 
-/Users/liangchaohuang/Desktop/MiniRevm_Simulation/rust_simulate_evm/target/debug/deps/liblib_utils-b075c16c6522fa08.rlib: crates/libs/lib_utils/src/lib.rs crates/libs/lib_utils/src/error.rs
+/Users/liangchaohuang/Desktop/zxc/MiniRevm_Simulation/rust_simulate_evm/target/debug/deps/liblib_utils-b075c16c6522fa08.rlib: crates/libs/lib_utils/src/lib.rs crates/libs/lib_utils/src/error.rs
 
-/Users/liangchaohuang/Desktop/MiniRevm_Simulation/rust_simulate_evm/target/debug/deps/lib_utils-b075c16c6522fa08.d: crates/libs/lib_utils/src/lib.rs crates/libs/lib_utils/src/error.rs
+/Users/liangchaohuang/Desktop/zxc/MiniRevm_Simulation/rust_simulate_evm/target/debug/deps/lib_utils-b075c16c6522fa08.d: crates/libs/lib_utils/src/lib.rs crates/libs/lib_utils/src/error.rs
 
 crates/libs/lib_utils/src/lib.rs:
 crates/libs/lib_utils/src/error.rs:
